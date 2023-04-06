a9bc45c9424ef0a175967881d8c4991bdbe093e1
Alexis Aguilar <alexmerona@gmail.com>
2023-04-05 20:33:12 -0400

    fixing styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="app bg-black text-white">
+    <div className="app h-screen bg-black text-white">
       <div className="container mx-auto flex flex-col items-center text-center">
         <Hero />
         <Links />
